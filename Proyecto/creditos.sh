a2e0b3a29678df3061b6576086478197e9e14164
Alejandro Marquina <alejandromarquina69@gmail.com>
2023-04-05 14:56:07 -0600

    Agregue el SH de los creditos

---
+++ Proyecto/creditos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Función para mostrar los créditos del programador
+function mostrar_creditos {
+    echo "==================================="
+    echo "           Créditos               "
+    echo "==================================="
+    echo "Programador : [Olivos Jimenez Luis Mario]"
+    echo "Email : [luismarioolivos05@gmail.com]"
+    echo "Git Hub : [https://github.com/LuisOJ]"
+    echo "Programador : [Marquina Vazquez Alejandro Jair]"
+    echo "Email : [correo electrónico]"
+    echo "Git Hub : [link]"
+    echo "==================================="
+}
+
+# Llamada a la función para mostrar los créditos del programador
+mostrar_creditos
