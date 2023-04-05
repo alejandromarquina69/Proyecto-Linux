4fe2fcb1d1a37a0a423f3002201c369badcff49b
Alejandro Marquina <alejandromarquina69@gmail.com>
2023-04-05 14:54:18 -0600

    Agregue el SH de ayuda

---
+++ Proyecto/ayuda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo "Comandos disponibles:"
+echo "ayuda: Muestra esta ayuda"
+echo "infosis: Muestra información del sistema"
+echo "fecha: Muestra la fecha actual"
+echo "buscar: Busca un archivo en una carpeta específica"
+echo "gato: Despliega el juego llamado gato"
+echo "reproductor: Despliega un reproductor mp3 con interfaz grafica"
+echo "creditos: Muestra los creditos de los programadores"
