286df041e270011066c8a6690ec35406301dbb29
Alejandro Marquina <alejandromarquina69@gmail.com>
2023-04-18 20:40:58 -0600

    Agregue codigo a juego GATO

--- Proyecto/gato.sh
+++ Proyecto/gato.sh
@@ -1,144 +1,128 @@
 #!/bin/bash
 
-trap 'echo -e "\033[00;30m \n\nGracias por jugar \n"; exit 127' SIGINT
-COLOR_RED='\033[01;31m'
-COLOR_GREEN='\033[01;32m'
-COLOR_BLACK='\033[00;30m'
-COLOR_BLACK_BOLD='\033[01;30m'
-DEFAULT_COL='\033[01;30m'
-   
-array=( "" "" "" "" "" "" "" "" "" )
-    
-    win_msg () {
-    echo -e "${COLOR_GREEN}Felicidades $USER_NO tu Ganas!!${COLOR_BLACK}\n"
-    break ;
-    }
-     
-     winning_rules () {
-      if [ $CELL_VALUE == "${array[0]}" ] && [ $CELL_VALUE == "${array[1]}" ] && [ $CELL_VALUE == "${array[2]}" ] ; then
-      win_msg
-      elif [ $CELL_VALUE == "${array[3]}" ] && [ $CELL_VALUE == "${array[4]}" ] && [ $CELL_VALUE == "${array[5]}" ] ; then
-      win_msg
-      elif [ $CELL_VALUE == "${array[6]}" ] && [ $CELL_VALUE == "${array[7]}" ] && [ $CELL_VALUE == "${array[8]}" ] ; then
-      win_msg
-      elif [ $CELL_VALUE == "${array[0]}" ] && [ $CELL_VALUE == "${array[3]}" ] && [ $CELL_VALUE == "${array[6]}" ] ; then
-      win_msg;
-      elif [ $CELL_VALUE == "${array[1]}" ] && [ $CELL_VALUE == "${array[4]}" ] && [ $CELL_VALUE == "${array[7]}" ] ; then
-      win_msg
-      elif [ $CELL_VALUE == "${array[2]}" ] && [ $CELL_VALUE == "${array[5]}" ] && [ $CELL_VALUE == "${array[8]}" ] ; then
-      win_msg
-      elif [ $CELL_VALUE == "${array[0]}" ] && [ $CELL_VALUE == "${array[4]}" ] && [ $CELL_VALUE == "${array[8]}" ] ; then
-      win_msg
-      elif [ $CELL_VALUE == "${array[2]}" ] && [ $CELL_VALUE == "${array[4]}" ] && [ $CELL_VALUE == "${array[6]}" ] ; then
-      win_msg
-      fi
-     }
-       
-  tie ()  {
-   for k in `seq 0 $( expr ${#array[@]} - 1) `
-    do
-     if [ ! -z ${array[$k]} ] ; then
-      tic_array[$k]=$k
-       if [ "9"  -eq ${#tic_array[@]} ] ; then
-        echo -e "${COLOR_GREEN}\nTal vez tengas mejor suerte !!${COLOR_BLACK}\n"
-        exit
-       fi 
-     fi
-    done
-  }
-        
-  tic_board () {
-   clear
-   echo -e "\t${COLOR_BLACK_BOLD}*************************"
-   echo -e "\t*\t ${array[0]:-0} | ${array[1]:-1} | ${array[2]:-2}\t*"
-   echo -e "\t*\t____________\t*"
-   echo -e "\t*\t ${array[3]:-3} | ${array[4]:-4} | ${array[5]:-5}\t*"
-   echo -e "\t*\t____________\t*"
-   echo -e "\t*\t ${array[6]:-6} | ${array[7]:-7} | ${array[8]:-8}\t*"
-   echo -e "\t*************************\n${COLOR_BLACK}"
-  }
-         
-  EMPTY_CELL () {
-   echo -e -n "${DEFAULT_COL}"
-   read -e -p "$MSG" col
-   case "$col" in
-    [0-8]) if [  -z ${array[$col]}   ]; then
-    CELL_IS=empty
-   else 
-    CELL_IS=notempty 
-   fi;;
-     *)  DEFAULT_COL=$COLOR_RED
-     MSG="$USER_NO Escribe un número entre 0 a 8 : "
-     EMPTY_CELL;;
-   esac
-   echo -e -n "${COLOR_BLACK}"
-  }
-          
-           
-  CHOISE () { 
-   EMPTY_CELL
-   if [ "$CELL_IS" == "empty" ]; then
-    array[$col]="$CELL_VALUE"
-   else  
-    DEFAULT_COL=${COLOR_RED}
-    MSG="La caja no puede estar vacía, Re-Enter $USER_NO : "
-    CHOISE 
-   fi
-  }
-            
-   PLAYER_NAME () {
-   if [ -z $USER1 ]; then
-    read -e -p "Introduce el nombre del primer jugador: " USER1
-   fi
-   
-   if [ -z $USER2 ]; then
-    read -e -p "Introduce el nombre del segundo jugador: " USER2
-   fi
-     
-   if [ -z $USER1 ] ; then 
-     echo -e "${COLOR_RED}El nombre del jugandor no puede estar vacío" 
-     PLAYER_NAME
-   elif [ -z $USER2 ]; then
-     echo -e "${COLOR_RED}El nombre del jugandor no puede estar vacío" 
-     PLAYER_NAME
-   fi
-   }
-     
-# Main Program
-tic_board
-echo -e "${DEFAULT_COL}Bienvenido al juego Tic Tac toe"
-echo -e "La reglas son, introducir un número en la caja entre 0 a 8"
-read -n 1 -p "Para continuar escribe y : " y
-echo -e "\n"
-sleep 1
-     
-    if  [ "$y" == "y" ]  ||  [ "$y" == "Y" ]; then
-     clear
-     echo -e "${COLOR_BLACK_BOLD}"
-     PLAYER_NAME
+jugador_1="X"
+jugador_2="O"
+
+turno=1
+juego_encendido=true
+
+juego=1
+
+movimiento=( 1 2 3 4 5 6 7 8 9 )
+bienvenida() {
+  clear
+  echo "========================"
+  echo "==========GATO=========="
+  echo "========================"
+  sleep 1
+}
+
+tablero() {
+  clear
+  echo " ${movimiento[0]} | ${movimiento[1]} | ${movimiento[2]} "
+  echo "-----------"
+  echo " ${movimiento[3]} | ${movimiento[4]} | ${movimiento[5]} "
+  echo "-----------"
+  echo " ${movimiento[6]} | ${movimiento[7]} | ${movimiento[8]} "
+  echo "============="
+}
+
+seleccion_jugador(){
+  # JUEGA LA PC
+  if [[ $juego -eq 1 ]]
+  then  
+   if [[ $(($turno % 2)) == 0 ]]
+    then
+      jugar=$jugador_2
+      casilla=$(( $RANDOM % 10 ))
+      echo $casilla
     else
-     echo -e "\nGracias por usarlo !!"
-     exit 
+      echo -n "JUGADOR 1 ESCOGE UNA CASILLA:"
+      jugar=$jugador_1
+      read casilla
     fi
-                
- tic_board
-  while :
-   do
-    ((i++))
-    value=`expr $i % 2`
-    if  [ "$value" == "0" ]; then
-     USER_NO=$USER1
-     MSG="$USER_NO Introduce tu elección : "
-     CELL_VALUE="X"
-     CHOISE 
-    else 
-     USER_NO=$USER2
-     MSG="$USER_NO Introduce tu elección : "
-     CELL_VALUE="O"
-     CHOISE 
-   fi
- 
-    tic_board
-    winning_rules
-    tie 
-  done
+  else
+    # JUEGA HUMANO
+    if [[ $(($turno % 2)) == 0 ]]
+    then
+      jugar=$jugador_2
+      echo -n "JUGADOR 2 ESCOGE UNA CASILLA:"
+      read casilla
+    else
+      jugar=$jugador_1
+      echo -n "JUGADOR 1 ESCOGE UNA CASILLA:"
+      read casilla
+    fi
+  fi
+  echo "no seleccionaste nada"
+
+  space=${movimiento[($casilla -1)]} 
+
+  if [[ $casilla =~ ^-[0-9]+$ ]] || [[ ! $space =~ ^[0-9]+$  ]]
+  then 
+    echo "No es una casilla valida."
+    seleccion_jugador
+  else
+    movimiento[($casilla -1)]=$jugar
+    ((turno=turno+1))
+  fi
+  space=${movimiento[($casilla-1)]} 
+}
+
+coicidencia() {
+  if  [[ ${movimiento[$1]} == ${movimiento[$2]} ]]&& \
+      [[ ${movimiento[$2]} == ${movimiento[$3]} ]]; then
+    juego_encendido=false
+  fi
+  if [ $juego_encendido == false ]; then
+    if [ ${movimiento[$1]} == 'X' ];then
+      echo "Jugador 1 gana!"
+      return 
+    else
+      echo "Jugador 2 gana!"
+      return 
+    fi
+  fi
+}
+
+checar_ganador(){
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 0 1 2
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 3 4 5
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 6 7 8
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 0 4 8
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 2 4 6
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 0 3 6
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 1 4 7
+  if [ $juego_encendido == false ]; then return; fi
+  coicidencia 2 5 8
+  if [ $juego_encendido == false ]; then return; fi
+
+  if [ $turno -gt 9 ]; then 
+    juego_encendido=false
+    echo "Empate!"
+  fi
+}
+
+bienvenida
+tablero
+echo "Quieres jugar con la computadora? [Y/n]"
+read quien
+echo $quien
+if [[ $quien == "n" ]]
+then
+  juego=0
+fi
+
+while $juego_encendido
+do
+  seleccion_jugador
+  tablero
+  checar_ganador
+done
