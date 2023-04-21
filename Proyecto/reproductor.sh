f018a20163baad4e6954d8ea90e27a4348627d92
“Luis <luismarioolivos05@gmail.com>
2023-04-20 20:13:18 -0600

    corregi el reproductor e hice una interfaz grafica

--- Proyecto/reproductor.sh
+++ Proyecto/reproductor.sh
@@ -1,121 +1,63 @@
 #!/bin/bash
-#Cree funcion reproducir
-reproducir(){
-#accedi a la ubicacion antes pedida
-cd "$r"
-#inicialice el comando mpg123
-mpg123 $r/*.mp3
-}
-#Cree la funcion lista
-lista(){
-#Borro la lista antes creada por que se creaba una antes pero le digo que no me muestre el error con &>/dev/null
-rm .lista &>/dev/null
-#Aqui lo que hago es mandar a llamar el comando ls y redireccionar a un nuevo doccumento pero que este se cree oculto
-ls>>.lista
-#con este comando creo la interfaz y le digo que imprima lo que dice lista
-whiptail --title "REPRODUCTOR DE MUSICA" \
-         --textbox .lista 20 80
-#mando a llamar el mismo comando pero le digo que lo que obtenga lo guarde
-op=$(whiptail --title "MENU" \
-               --menu "Elige una opción" 10 80 2 \
-               "LISTA" "MUESTRA LA LISTA DE CANCIONES" \
-               "INSTRUCCIONES" "MUESTRA LAS INTRUCCIONES" \
-	       "REPRODUCIR" "INICIALIZA EL REPRODUCTOR" \
-               3>&1 1<&2 2>&3)
-#la opcion que toma la mando a llamar y la meto en un case para saber que hacer
-case $op in
-	"LISTA")
-	cd "$r" &>/dev/null
-	rm .lista &>/dev/null
-	lista
-	;;
-	"REPRODUCIR")
-	reproducir
-	;;
-	"INSTRUCCIONES")
-	instrucciones
-	;;
-esac
-}
-#cree la funcion instrucciones
-instrucciones(){
-#mando a llamar una interfaz y le digo como funciona mpg123
-whiptail --title "INSTRUCCIONES"\
-	 --msgbox "PRESIONA h en la terminal cuando se reprodusca la musica" 10 80
-#aqui lo que hago es volver a llamar el menu para que cuando de una opcion en vez de salir a la terminal pueda ver las demas opciones
-i=$(whiptail --title "MENU" \
-               --menu "Elige una opción" 10 80 2 \
-               "LISTA" "MUESTRA LA LISTA DE CANCIONES" \
-	       "INSTRUCCIONES" "MUESTRA LAS INTRUCCIONES" \
-               "REPRODUCIR" "INICIALIZA EL REPRODUCTOR" \
-               3>&1 1<&2 2>&3)
-case $i in
-	"LISTA")
-	instrucciones
-	;;
-	"REPRODUCIR")
-	reproducir
-	;;
-	"INSTRUCCIONES")
-	instrucciones
-	;;
-esac
-}
-#con esta funcion pido los datos para poder correr las demas funciones (pide ruta)
-pedir(){
-echo "Dime la ruta donde tienes tu musica con el siguiente formato"
-echo "/ruta"
-read r
-cd "$r" &>/dev/null
-if [ $? -eq 0 ];then
-	mp3
-else
-	echo
-	echo "La ruta que me diste esta mal"
-	pedir
+
+# Comprobar si mpg123 está instalado
+if ! command -v mpg123 &> /dev/null; then
+    echo "mpg123 no está instalado"
+    read -p "¿Desea instalarlo? (s/n) " option
+    if [ "$option" = "s" ]; then
+        # Instalar mpg123
+        sudo apt-get install mpg123
+    else
+        echo "Saliendo del programa"
+        exit
+    fi
 fi
-}
-#manda a llamar la interfaz principal
-mp3(){
-op=$(whiptail --title "MENU" \
-               --menu "Elige una opción" 10 80 2 \
-               "LISTA" "MUESTRA LA LISTA DE CANCIONES" \
-	       "INSTRUCCIONES" "MUESTRA INSTRUCCIONES" \
-               "REPRODUCIR" "INICIALIZA EL REPRODUCTOR" \
-               3>&1 1<&2 2>&3)
-case $op in
-	"LISTA")
-	lista
-	;;
-	"REPRODUCIR")
-	reproducir
-	;;
-	"INSTRUCCIONES")
-	instrucciones
-	;;
-esac
-}
-#en este verificamos si tiene instalado el paquete mpg123
-Verificar(){
-dpkg -l	| grep "mpg123"> /dev/null
-if [ $? -eq 0 ];then
-	pedir
-else
-	echo "Deseas instalar el paquete necesario para que el programa corra? (y/n) "
-	read op
-	case $op in
-		y)
-		#instalamos
-		sudo apt-get update
-		sudo apt-get install mpg123
-		;;
-		n)
-		;;
-		*)
-		echo "LO SIENTO DESCONOZCO ESA OPCION"
-		Verificar
-		;;
-	esac
+
+# Listar archivos MP3 en la carpeta actual
+mp3_files=(*.mp3)
+
+if [ ${#mp3_files[@]} -eq 0 ]; then
+    echo "No se encontraron archivos MP3 en la carpeta actual"
+    exit 1
 fi
-}
-Verificar
+
+# Mostrar lista de canciones en un cuadro de diálogo
+selected_song=$(whiptail --title "Reproductor de MP3" --menu "Seleccione una canción:" 15 60 4 "${mp3_files[@]}" 3>&1 1>&2 2>&3)
+
+
+
+# Iniciar el reproductor con la canción seleccionada
+mpg123 "$selected_song"
+
+# Bucle para permitir la navegación por la biblioteca musical
+while true; do
+    # Mostrar información sobre la canción actual
+    echo "Reproduciendo: $selected_song"
+
+    # Leer la entrada del usuario
+    read -p "Opciones: [n] Siguiente canción, [p] Canción anterior, [q] Salir " option
+
+    # Procesar la entrada del usuario
+    case $option in
+        n)
+            # Mostrar lista de canciones en un cuadro de diálogo
+            selected_song=$(whiptail --title "Reproductor de MP3" --menu "Seleccione una canción:" 15 60 4 "${mp3_files[@]}" 3>&1 1>&2 2>&3)
+            # Reproducir la siguiente canción en la lista
+            mpg123 "$selected_song"
+            ;;
+        p)
+            # Mostrar lista de canciones en un cuadro de diálogo
+            selected_song=$(whiptail --title "Reproductor de MP3" --menu "Seleccione una canción:" 15 60 4 "${mp3_files[@]}" 3>&1 1>&2 2>&3)
+            # Reproducir la canción anterior en la lista
+            mpg123 "$selected_song"
+            ;;
+        q)
+           # Salir del reproductor
+            echo "Saliendo del reproductor de MP3"
+            exit
+            ;;
+        *)
+            echo "Opción inválida"
+            ;;
+    esac
+done
