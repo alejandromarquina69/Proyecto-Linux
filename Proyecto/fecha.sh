92f94d5b427aed6878298f52ad652c8c28def600
Alejandro Marquina <alejandromarquina69@gmail.com>
2023-04-21 11:06:17 -0600

    Agregue lista de canciones

--- Proyecto/fecha.sh
+++ Proyecto/fecha.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+timedatectl set-local-rtc 1
 
-echo "La fecha y hora actual son: $(date +"%Y-%m-%d %H:%M:%S")"
+echo "La fecha de hoy es:"
+cat /proc/driver/rtc |grep 'rtc_date' |grep '[0-9]*-[0-9]*-[0-9]*' -o
+
+echo "La hora actual es:"
+cat /proc/driver/rtc |grep 'rtc_time' |grep '[0-9]*:[0-9]*:[0-9]*' -o
